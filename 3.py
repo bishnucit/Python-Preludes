7c7381697959b00c66a828d47290b1d47f1f04c7
Bishnu <bishnucit@gmail.com>
2015-01-28 15:16:48 +0530

    1
    1

--- 3.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-# clicking on Welcome link.
-driver = webdriver.Firefox()
-driver.get("http://www.practiceselenium.com/")
-driver.find_element_by_link_text("Our Passion").click()
-assert "Our Passion" in driver.title
-driver.close()
