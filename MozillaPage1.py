acb96ed3a3e83997b0e4da815878081129b77e8b
Bishnu <bishnucit@gmail.com>
2015-05-29 18:15:48 +0530

    Update 4
    Minor Fixes Code works perfect.

--- MozillaPage1.py
+++ MozillaPage1.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-
+from selenium.webdriver.common.keys import Keys
 
 
 driver = webdriver.Firefox()
@@ -7,8 +7,7 @@ driver.get("https://marketplace-dev.allizom.org/")
 driver.implicitly_wait(12)
 driver.find_element_by_xpath("//*[@id='site-header']/mkt-header-child-toggle[1]").click()
 driver.implicitly_wait(2)
-driver.find_element_by_xpath("//*[@id='search-q']").send_keys("Hello")
-driver.send_keys(Keys.RETURN)
+driver.find_element_by_xpath("//*[@id='search-q']").send_keys("Hello"+ Keys.RETURN)
 assert "Hello | Firefox Marketplace" in driver.title
 
 driver.close()
