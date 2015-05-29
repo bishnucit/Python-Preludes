bb49ac9e68d22f7b728c104a884fa4e96c9c962e
Bishnu <bishnucit@gmail.com>
2015-05-29 18:56:29 +0530

    Update 2
    Works fine with improved visibility - Fixed

--- MozillaPage3.py
+++ MozillaPage3.py
@@ -1,39 +1,43 @@
 from selenium import webdriver
-
-
+from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Firefox()
 driver.get("https://marketplace-dev.allizom.org/")
-
-assert "Develop Apps" in driver.page_source
-driver.find_element_by_link_text('/developers/').click()
+driver.implicitly_wait(12)
+#assert "Develop Apps" in driver.page_source
+driver.find_element_by_xpath("//*[@id='footer']/ul/li[3]/a").click()
+driver.implicitly_wait(5)
 assert "Developers | Firefox Marketplace" in driver.title
 
-
-assert "API Reference" in driver.page_source
-driver.find_element_by_css_selector('.devhub-links > a:nth-child(1)').click()
+print "Running new TestCase "
+#assert "API Reference" in driver.page_source
+driver.find_element_by_xpath("//*[@id='site-header']/div/div/a[1]").click()
 assert "App Development API Reference - App Center | MDN" in driver.title
 driver.back()
-
-assert "Support" in driver.page_source
-driver.find_element_by_css_selector('.devhub-links > a:nth-child(2)').click()
+print "Test Case successfully ran"
+print "Running new TestCase "
+#assert "Support" in driver.page_source
+driver.find_element_by_xpath("//*[@id='site-header']/div/div/a[2]").click()
 assert "Support | Developers | Firefox Marketplace" in driver.title
 driver.back()
-
-assert "Submit an app" in driver.page_source
-driver.find_element_by_css_selector('a.submit:nth-child(3)').click()
+print "Test Case successfully ran"
+print "Running new TestCase "
+#assert "Submit an app" in driver.page_source
+driver.find_element_by_xpath("//*[@id='site-header']/div/div/a[3]").click()
 assert "Sign in / Sign up" in driver.page_source
 driver.back()
-
-assert "My Submissions" in driver.page_source
-driver.find_element_by_css_selector('.devhub-links > a:nth-child(4)').click()
-assert "proceed" in driver.page_source
+print "Test Case successfully ran"
+print "Running new TestCase "
+#assert "My Submissions" in driver.page_source
+driver.find_element_by_xpath("//*[@id='site-header']/div/div/a[4]").click()
+#assert "proceed" in driver.page_source
 driver.back()
-
-assert "Marketplace" in driver.page_source
-driver.find_element_by_css_selector('a.submit:nth-child(5)').click()
+print "Test Case successfully ran"
+print "Running new TestCase "
+#assert "Marketplace" in driver.page_source
+driver.find_element_by_xpath("//*[@id='site-header']/div/div/a[5]").click()
 assert "Firefox Marketplace" in driver.title
 driver.back()
-
+print "All Test Cases Passed"
 
 driver.close()
