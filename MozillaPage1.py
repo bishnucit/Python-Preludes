73e667102974014e33118cff72b213cb1f022b07
Bishnu <bishnucit@gmail.com>
2015-05-29 18:07:27 +0530

    Editing send keys
    Wrong send keys code - fixed

--- MozillaPage1.py
+++ MozillaPage1.py
@@ -8,7 +8,7 @@ driver.implicitly_wait(12)
 driver.find_element_by_xpath("//*[@id='site-header']/mkt-header-child-toggle[1]").click()
 driver.implicitly_wait(2)
 driver.find_element_by_xpath("//*[@id='search-q']").send_keys("Hello")
-driver.find.send_keys(Keys.RETURN)
+driver.send_keys(Keys.RETURN)
 assert "Hello | Firefox Marketplace" in driver.title
 
 driver.close()
